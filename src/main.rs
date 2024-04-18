d3ab3fdd78e0765ac76471ba9ebb4b9a9fc1bbfd
Ben Dowling <bendowlingtech@gmail.com>
2024-04-18 22:03:54 +0100

    first commit

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+mod handlers;
+mod models;
+
+use axum::{
+    routing::{get, post},
+    http::StatusCode,
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
+
+#[tokio::main]
+async fn main() {
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    // build our application with a route
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/users", post(handlers::create_post));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
